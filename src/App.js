1d609a47c361109ebd1558c4f60884b82ef2dd49
marcusty15 <marco.troconis15@gmail.com>
2022-06-25 12:39:21 -0400

    prueba

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import About from './components/About/About';
+import Carrusel from './components/Carrusel/Carrusel';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
    <>
    <Navbar/>
+   <About/>
+   <Carrusel/>
    </>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,40 +1,27 @@
 import React from "react";
 import './Navbar.css'
+import imagen from "../../img/foto3.png"
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-light a">
-      <div className="container-fluid  b">
-        <a className="navbar-brand " href="#">
-          Navbar
+    <nav className="navbar navbar-expand-lg  bg-dark">
+    <div className="container-fluid">
+        <a href="#" className="navbar-brand">
+            <img src={imagen} height="28" alt="CoolBrand"/>
         </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
+        <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Link
-              </a>
-            </li>
-          </ul>
+        <div className="collapse navbar-collapse justify-content-center" id="navbarCollapse">
+            <div className="navbar-nav">
+                <a href="#" className="nav-item nav-link active me-2 item">Sobre MI</a>
+                <a href="#" className="nav-item nav-link me-2">Cortes</a>
+                <a href="#" className="nav-item nav-link me-2">Ubicacion</a>
+                <a href="#" className="nav-item nav-link " tabIndex="-1">Contacto</a>
+            </div>
         </div>
-      </div>
-    </nav>
+    </div>
+</nav>
   );
 };
 
